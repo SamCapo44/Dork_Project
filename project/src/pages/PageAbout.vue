802ce59d42869b3c70de4b6a31a725f95a7a0f7b
SamCapo44 <samcapo44@gmail.com>
2021-09-29 06:10:27 -0700

    newpro

---
+++ project/src/pages/PageAbout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Dork</h4>
+    <div class="text-body1">
+    <p>We are a social platform that's designed to help small and large businesses reach customers and grow their brands. 
+    From start to finish we guide you through the process so nothing gets lost in translation. 
+    We work with your team and can even help find influencers for your product or service.
+    Our Platform help you connect with friends and family online. With our platform, you can easily share photos, 
+    videos and chat with your contacts. You also have the opportunity to discover new music, 
+    movies or TV shows recommended just for you by your friends. We usually offer a range 
+    of privacy controls so you can customize and connect with your audience for each post depending.
+    </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
---
+++ project/src/pages/PageHome.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-page class="relative-position">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            v-model="newDorkContent"
+            class="new-dork"
+            placeholder="What's happening?"
+            maxlength="280"
+            bottom-slots
+            counter
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewDork"
+            :disable="!newDorkContent"
+            class="q-mb-lg"
+            color="primary"
+            label="Dork"
+            rounded
+            unelevated
+            no-caps
+          />
+        </div>
+      </div>
+
+     <q-separator  
+      class="divider"
+      size="10px"
+      color="grey-2"
+     />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageHome',
+  data() {
+    return {
+      newDorkContent: ''
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.new-dork
+  textarea
+    font-size: 19px
+    line-height: 1.44 !important
+  .divider
+    boreder-top: 1px solid
+    boreder-bottom: 1px solid
+    boreder-color: $grey-4
+</style>
